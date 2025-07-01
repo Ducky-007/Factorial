8b10c1a525afc95714323eb5e2f7460d6012b734
Cameron Duckett <114952118+Ducky-007@users.noreply.github.com>
2025-07-01 11:09:45 -0700

    adds project file

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,26 @@
+﻿// app for finding factorial of an inputted number
+
+using System;
+
+class factorial
+{
+    public static void Main()
+    {
+        Console.Write("Enter number: "); // prompt user to enter number
+        int num = int.Parse(Console.ReadLine()); // save an int variable
+        Console.Write($"Number Factorial is: {Factorial(num)}"); // display num back to user
+    }
+
+    // factorial method
+    static int Factorial(int num)
+    {
+        if (num <= 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return num * Factorial(num - 1);
+        }
+    }
+}
